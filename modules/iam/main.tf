ce177c463c1d50fe736401a68d5dc6599874e48d
jungyunsoo0713 <young@apipen.com>
2026-05-31 10:40:41 +0900

    feat: multi-service ECS deployment

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -95,11 +95,13 @@ resource "aws_iam_role_policy" "github_actions" {
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:HeadObject",
+          "s3:HeadBucket"
         ]
         Resource = [
-          "arn:aws:s3:::your-tfstate-bucket-name",
-          "arn:aws:s3:::your-tfstate-bucket-name/*"
+          "arn:aws:s3:::y-so-cereal-tfstate-ap-northeast-2",
+          "arn:aws:s3:::y-so-cereal-tfstate-ap-northeast-2/*"
         ]
       },
       # Terraform DynamoDB lock
